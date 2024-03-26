2c272aa2f31dd2d3fca59df6de0db1dd93d572d0
Brian Seeley <bseeley1@uncc.edu>
2024-03-26 18:01:31 -0400

    Background Image

--- Lua Tank/main.lua
+++ Lua Tank/main.lua
@@ -5,3 +5,7 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+local background = display.newImageRect("background.png", 570, 360)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
