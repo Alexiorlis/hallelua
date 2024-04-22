11356e4f155374d41e263c844ff670e88fd1560f
Alexiorlis <alexiorlis97@gmail.com>
2024-04-22 13:28:00 -0400

    update game.ua
    took out commented code and fixed map

--- myEdit/scene/game.lua
+++ myEdit/scene/game.lua
@@ -1,12 +1,8 @@
---***THIS IS WHERE OTHER OBJECTS WILL BE IMPORTED AND SET-- i think
-
-
 --Include libraries
 local composer = require("composer")
 local fx = require("com.ponywolf.ponyfx")
 local physics = require("physics")
 local json = require( "json" )
---change to "blueTank" later. this is just for reference for now
 local blueTank = require("scene.game.lib.blueTank")
 local redTank = require("scene.game.lib.redTank")
 
@@ -24,22 +20,6 @@ function scene:create(event)
     --add scene display objects
     local sceneGroup = self.view
 
-    --This is where sounds would be added but muted for now since i have not created soundtrack
-    -- local sndDir = "scene/game/sfx/"
-	-- scene.sounds = {
-	-- 	thud = audio.loadSound( sndDir .. "thud.mp3" ),
-	-- 	sword = audio.loadSound( sndDir .. "sword.mp3" ),
-	-- 	squish = audio.loadSound( sndDir .. "squish.mp3" ),
-	-- 	slime = audio.loadSound( sndDir .. "slime.mp3" ),
-	-- 	wind = audio.loadSound( sndDir .. "loops/spacewind.mp3" ),
-	-- 	door = audio.loadSound( sndDir .. "door.mp3" ),
-	-- 	hurt = {
-	-- 		audio.loadSound( sndDir .. "hurt1.mp3" ),
-	-- 		audio.loadSound( sndDir .. "hurt2.mp3" ),
-	-- 	},
-	-- 	hit = audio.loadSound( sndDir .. "hit.mp3" ),
-	-- 	coin = audio.loadSound( sndDir .. "coin.mp3" ),
-    -- }
 
     --Start physics
 	physics.start()
@@ -69,7 +49,7 @@ function scene:create(event)
 			for j=0,9 do
 				if (i==0 and (j==0 or j==1 or j==5 or j==6)) or
 				(i==1 and (j==4)) or (i==2 and (j==2)) or (i==3 and (j==3 or j==6)) or
-				(i==4 and (j==0 or j==1 or j==5)) or (i==5 and (j==5)) or (i==6 and (j==2 or j==5)) or
+				-- (i==4 and (j==0 or j==1 or j==5)) or (i==5 and (j==5)) or (i==6 and (j==2 or j==5)) or
 				(i==7 and (j==0)) or (i==8 and (j==4)) or (i==9 and (j==2 or j==3)) or
 				(i==11 and (j==0 or j==1 or j==6 or j==7)) or (i==12 and (j==2)) then
 					for k=1,4 do
@@ -99,22 +79,6 @@ function scene:create(event)
 
 	createWalls()
 
--- Your code here
-
-	
-
-    -- Load our map
-    --***This is for loading the map that is created and NOT the background
-	-- local filename = event.params.map or "scene/game/map/sandbox.json"
-	-- local mapData = json.decodeFile( system.pathForFile( filename, system.ResourceDirectory ) )
-	-- map = tiled.new( mapData, "scene/game/map" )
-
-    -- --**This loads the blueTank on the map, but is not needed yet since I am hardcoding for testing
-    -- map.extensions = "scene.game.lib."
-	-- map:extend("blueTank")
-	-- blueTank = map:findObject("blueTank")
-	-- blueTank.filename = filename
-    --calling blueTank (hardcoded)
     local blueTank = blueTank.new(display.newImageRect("scene/game/img/blueTank.png", 80, 80), 180)
 	local redTank = redTank.new(display.newImageRect("scene/game/img/redTank.png", 80, 80), 0)
 end
